034c80d57aba108593ea9653f358e270d49e4a2d
Lunark :3 <jvillaltamatos3007@gmail.com>
2026-02-14 13:57:05 -0500

    drivers: Import Ksu-next 3.1.0
    Change-Id: I2dd0030e2a72b2d21ce771284c99ec2a4c63b6ba
    Signed-off-by: Lunark :3 <jvillaltamatos3007@gmail.com>

---
+++ drivers/kernelsu/ksu.c
@@ -0,0 +1,117 @@
+#include <linux/export.h>
+#include <linux/fs.h>
+#include <linux/kobject.h>
+#include <linux/module.h>
+#include <linux/workqueue.h>
+
+#include "allowlist.h"
+#include "feature.h"
+#include "klog.h" // IWYU pragma: keep
+#include "throne_tracker.h"
+#include "syscall_hook_manager.h"
+#include "ksud.h"
+#include "supercalls.h"
+#include "ksu.h"
+#include "file_wrapper.h"
+
+// workaround for A12-5.10 kernel
+// Some third-party kernel (e.g. linegaeOS) uses wrong toolchain, which supports
+// CC_HAVE_STACKPROTECTOR_SYSREG while gki's toolchain doesn't.
+// Therefore, ksu lkm, which uses gki toolchain, requires this __stack_chk_guard,
+// while those third-party kernel can't provide.
+// Thus, we manually provide it instead of using kernel's
+#if defined(CONFIG_STACKPROTECTOR) && !defined(CONFIG_STACKPROTECTOR_PER_TASK)
+#include <linux/stackprotector.h>
+#include <linux/random.h>
+unsigned long __stack_chk_guard __ro_after_init
+    __attribute__((visibility("hidden")));
+#define NO_STACK_PROTECTOR_WORKAROUND __attribute__((no_stack_protector))
+#else
+#define NO_STACK_PROTECTOR_WORKAROUND
+#endif
+
+struct cred *ksu_cred;
+
+NO_STACK_PROTECTOR_WORKAROUND
+int __init kernelsu_init(void)
+{
+#if defined(CONFIG_STACKPROTECTOR) && !defined(CONFIG_STACKPROTECTOR_PER_TASK)
+    unsigned long canary;
+
+    /* Try to get a semi random initial value. */
+    get_random_bytes(&canary, sizeof(canary));
+    canary ^= LINUX_VERSION_CODE;
+    canary &= CANARY_MASK;
+    __stack_chk_guard = canary;
+#endif
+
+#ifdef CONFIG_KSU_DEBUG
+	pr_alert("*************************************************************");
+	pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
+	pr_alert("**                                                         **");
+	pr_alert("**         You are running KernelSU in DEBUG mode          **");
+	pr_alert("**                                                         **");
+	pr_alert("**     NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE NOTICE    **");
+	pr_alert("*************************************************************");
+#endif
+
+    ksu_cred = prepare_creds();
+    if (!ksu_cred) {
+        pr_err("prepare cred failed!\n");
+    }
+
+	ksu_feature_init();
+
+	ksu_supercalls_init();
+
+	ksu_syscall_hook_manager_init();
+
+	ksu_allowlist_init();
+
+	ksu_throne_tracker_init();
+
+	ksu_ksud_init();
+
+    ksu_file_wrapper_init();
+
+#ifdef MODULE
+#ifndef CONFIG_KSU_DEBUG
+	kobject_del(&THIS_MODULE->mkobj.kobj);
+#endif
+#endif
+	return 0;
+}
+
+extern void ksu_observer_exit(void);
+void kernelsu_exit(void)
+{
+	ksu_allowlist_exit();
+
+	ksu_throne_tracker_exit();
+
+	ksu_observer_exit();
+
+	ksu_ksud_exit();
+
+	ksu_syscall_hook_manager_exit();
+
+	ksu_supercalls_exit();
+
+	ksu_feature_exit();
+
+	if (ksu_cred) {
+		put_cred(ksu_cred);
+	}
+}
+
+module_init(kernelsu_init);
+module_exit(kernelsu_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("weishu");
+MODULE_DESCRIPTION("Android KernelSU");
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 13, 0)
+MODULE_IMPORT_NS("VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver");
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
+MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
+#endif
