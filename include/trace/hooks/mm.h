BayLibre/android_kernel_common

ANDROID: vendor_hooks: Add hook to precisely control readahead
Add vendor hooks to precisely control readahead based on various
factors, such as filesystem type, file type, process priority, and
other considerations.

Bug: 387208968
Change-Id: I3016c5e0da487ae03b760dce4f6dbc0d90f145b3
Signed-off-by: Yang Yang <yang.yang@vivo.com>

include/trace/hooks/mm.h
@@ -385,6 +385,14 @@ DECLARE_HOOK(android_vh_lruvec_del_folio,
 		bool *skip),
 	TP_ARGS(lruvec, folio, lru, skip));
 
+DECLARE_HOOK(android_vh_do_async_mmap_readahead,
+	TP_PROTO(struct vm_fault *vmf, struct folio *folio, bool *skip),
+	TP_ARGS(vmf, folio, skip));
+
+DECLARE_HOOK(android_vh_do_sync_mmap_readahead,
+	TP_PROTO(struct vm_fault *vmf, struct file_ra_state *ra),
+	TP_ARGS(vmf, ra));
+
 DECLARE_HOOK(android_vh_cma_debug_show_areas,
 	TP_PROTO(bool *show),
 	TP_ARGS(show));

xiaomi-sm8550-devs/android_kernel_xiaomi_sm8550

Bug: 514596244
(cherry picked from commit d005368a82ecc5b4f7e04d9df7d007ef384bcd2f)

include/trace/hooks/mm.h
@@ -157,6 +157,9 @@ DECLARE_HOOK(android_vh_mem_cgroup_css_offline,
 DECLARE_HOOK(android_vh_vmpressure,
 	TP_PROTO(struct mem_cgroup *memcg, bool *bypass),
 	TP_ARGS(memcg, bypass));
+DECLARE_HOOK(android_vh_do_async_mmap_readahead,
+	TP_PROTO(struct vm_fault *vmf, struct page *page, bool *skip),
+	TP_ARGS(vmf, page, skip));
 DECLARE_HOOK(android_vh_do_page_trylock,
 	TP_PROTO(struct page *page, struct rw_semaphore *sem,
 		bool *got_lock, bool *success),